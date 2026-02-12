1425ecbc03ebe59b1d40c8d4843b5abf5bf82d35
DON <167806587+donoliva2@users.noreply.github.com>
2026-02-12 14:46:54 +0530

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,60 @@
+Config = {}
+
+Config.ValentineItems = {
+    ["roseflower"] = {
+        animDict = "anim@heists@humane_labs@finale@keycards",
+        animName = "ped_a_enter_loop",
+        prop = "prop_single_rose",
+        propBone = 18905,
+        propPlacement = { 0.13, 0.15, 0.0, -100.0, 0.0, -20.0 }
+    },
+    ["bigballoon"] = {
+        animDict = "anim@heists@humane_labs@finale@keycards",
+        animName = "ped_a_enter_loop",
+        prop = "heart_balloon",
+        propBone = 60309,
+        propPlacement = { 0.25, -0.84, 0.53, -157.2041, -101.1702, 28.0243 }
+    },
+    ["smallballoon"] = {
+        animDict = "anim@heists@humane_labs@finale@keycards",
+        animName = "ped_a_enter_loop",
+        prop = "pride_heart_balloon",
+        propBone = 60309,
+        propPlacement = { 1.0600, 0.4900, 0.0140, 73.5103, -30.9638, 19.3946 }
+    },
+    ["flowerbouquet"] = {
+        animDict = "hold_flowers@dad",
+        animName = "hold_flowers_clip",
+        prop = "prop_snow_flower_02",
+        propBone = 60309,
+        propPlacement = { 0.1100, -0.1300, 0.1200, -18.7186, 121.0529, -11.7724 }
+    },
+    ["rosebouquet"] = {
+        animDict = "hold_flowers@dad",
+        animName = "hold_flowers_clip",
+        prop = "pata_freevalentinesday3",
+        propBone = 28422,
+        propPlacement = { -0.0100, 0.0300, -0.1700, -6.0697, 60.1852, 3.4934 }
+    },
+    ["lovecake"] = {
+        animDict = "anim@heists@box_carry@",
+        animName = "idle",
+        prop = "bzzz_prop_cake_love_001",
+        propBone = 18905,
+        propPlacement = { 0.28, 0.06, 0.2, -54.0, -72.0, -6.0 }
+    },
+    ["pourpleheart"] = {
+        animDict = "anim@heists@box_carry@",
+        animName = "idle",
+        prop = "brum_heart",
+        propBone = 28422,
+        propPlacement = { -0.5600, 0.0240, -0.3690, -10.0000, 0.0000, -0.0000 }
+    },
+    ["teddybear"] = {
+        animDict = "impexp_int-0",
+        animName = "mp_m_waremech_01_dual-0",
+        prop = "v_ilev_mr_rasberryclean",
+        propBone = 24817,
+        propPlacement = { -0.20, 0.46, -0.016, -180.0, -90.0, 0.0 }
+    },
+} 
