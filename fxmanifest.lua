cde167c859bf152b7968f178b658eac5e6041e11
DON <167806587+donoliva2@users.noreply.github.com>
2026-02-12 14:47:20 +0530

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+aauthor 'DONxDEV'
+description 'This script is designed and created with the expertise of DON.'
+version 'N/A'
+
+client_scripts {
+    'config.lua',
+    'client/main.lua'
+}
+
+server_scripts {
+    'config.lua',
+    'server/main.lua'
+}
